3ab5d0de1bd3fc93b79b438bb0cfdcaaed708b93
302Blue <abaldwin@udel.edu>
2019-10-30 13:58:26 -0400

    Changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,17 @@
 import * as functions from 'firebase-functions';
+import express = require('express');
+import cors = require('cors');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(cors({ origin: true }));
+
+
+app.get('/users/:id', (req, res) => res.send(`GET /api/users/${req.params.id} here`));
+app.post('/users', (req, res) => res.send(`POST here`));
+app.put('/users/:id', (req, res) => res.send(`PUT here`));
+app.delete('/users/:id', (req, res) => res.send(`DELETE here`));
+app.get('/users', (req, res) => res.send(`GET users here`));
+
+// Expose Express API as a single Cloud Function:
+exports.api = functions.https.onRequest(app);
